818cd837b563894a93d0eabf325042ea1bfc441a
Andhika <91180519+AndhikaBukh@users.noreply.github.com>
2022-06-24 22:23:54 +0700

    Added App to index

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from './home';
+import { Landing } from './landing';
+import { Login } from './login';
+import { Signup } from './signup';
 
 const root = ReactDOM.createRoot(
-  document.querySelector(".root") as HTMLElement
+	document.querySelector('.root') as HTMLElement
 );
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+
+const App = () => {
+	return (
+		<div className="app">
+			<Signup />
+
+			{/* <BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Landing />} />
+					<Route path="login" element={<Login />} />
+					<Route path="home" element={<Home />} />
+				</Routes>
+			</BrowserRouter> */}
+		</div>
+	);
+};
+
+root.render(<React.StrictMode>{App()}</React.StrictMode>);
