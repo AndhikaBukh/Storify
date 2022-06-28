1454ca90e82d7aa88454db3b062c048aed442024
Andhika <91180519+AndhikaBukh@users.noreply.github.com>
2022-06-28 11:26:42 +0700

    Initial Commit

---
+++ frontend/src/massage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../components/button/button';
+import './index.css';
+
+export const Profile = () => {
+	return (
+		<div className="chat">
+			{/* HTML Goes here */}
+		</div>
+	);
+};
---
+++ frontend/src/profile/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../components/button/button';
+import './index.css';
+
+export const Profile = () => {
+	return <div className="profile"></div>;
+};
