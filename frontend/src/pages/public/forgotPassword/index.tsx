9afae9a2c08c4fc656d1e489c04592adc76c2595
Andhika <91180519+AndhikaBukh@users.noreply.github.com>
2022-07-08 18:33:25 +0700

    Optional Page - Fishined

--- frontend/src/pages/public/forgotPassword/index.tsx
+++ frontend/src/pages/public/forgotPassword/index.tsx
@@ -1,3 +1,61 @@
+import React from 'react';
+import './index.css';
+import { Navbar } from '../../../components/navbar/navbar';
+import { QuestionCircleIcon, UserIcon } from '../../../components/icons';
+import { Link, useNavigate } from 'react-router-dom';
+import { Input } from '../../../components/input/input';
+import { Button } from '../../../components/button/button';
+
 export const ForgotPasswordPage = () => {
-	return <div className="ForgotPasswordPage"></div>;
+	const navigate = useNavigate();
+
+	return (
+		<div className="forgot-password">
+			<Navbar
+				type="page"
+				pageNavbarAttributes={{
+					pageTitle: 'Forgot Password',
+					handlePageIconLeft: () => navigate('/login'),
+					pageIconRight: <QuestionCircleIcon />,
+					handlePageIconRight: () => {
+						navigate('/help');
+					},
+				}}
+			/>
+
+			<div className="forgot-password__container">
+				<div className="forgot-password__container__header">
+					<div className="forgot-password__container__header__title">
+						Please Enter Email Address
+					</div>
+					<div className="forgot-password__container__header__subtitle">
+						You&apos;ll receive Link via Email and reset your
+						password
+					</div>
+				</div>
+
+				<div className="forgot-password__container__form">
+					<Input
+						type="email"
+						placeholder="Email"
+						icon={<UserIcon color="#776BF8" />}
+					/>
+				</div>
+
+				<div className="forgot-password__container__help">
+					Forget Email Address?
+					<Link
+						className="forgot-password__container__help__link"
+						to="/help"
+					>
+						Help Center
+					</Link>
+				</div>
+			</div>
+
+			<div className="forgot-password__button">
+				<Button>Send Verivication Code</Button>
+			</div>
+		</div>
+	);
 };
--- frontend/src/pages/public/resetPassword/index.tsx
+++ frontend/src/pages/public/resetPassword/index.tsx
@@ -1,3 +1,83 @@
+import { useState } from 'react';
+import './index.css';
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from '../../../components/button/button';
+import {
+	PenIcon,
+	UserIcon,
+	PassIcon,
+	EyeIcon,
+	EyeSlashIcon,
+} from '../../../components/icons';
+import { Input } from '../../../components/input/input';
+import { Navbar } from '../../../components/navbar/navbar';
+import { Seperator } from '../../../components/seperator/seperator';
+
 export const ResetPasswordPage = () => {
-	return <div className="ResetPasswordPage"></div>;
+	const [showPassword, setShowPassword] = useState<boolean>(false);
+	const [passwordIcon, setPasswordicon] = useState<JSX.Element>(
+		<EyeSlashIcon />
+	);
+
+	const handleShowPassword = () =>
+		showPassword !== false
+			? setPasswordicon(<EyeSlashIcon />)
+			: setPasswordicon(<EyeIcon />);
+
+	const navigate = useNavigate();
+
+	return (
+		<div className="reset-password">
+			<Navbar
+				type="page"
+				pageNavbarAttributes={{
+					pageTitle: 'Reset Password',
+					handlePageIconLeft: () => {
+						navigate('/login');
+					},
+				}}
+			/>
+
+			<div className="reset-password__input-container">
+				<header className="reset-password__input-header">
+					<h1 className="reset-password__input-header-title">
+						Create New Password
+					</h1>
+				</header>
+
+				<Input
+					icon={<PassIcon color="#776bf8" />}
+					eventIcon={passwordIcon}
+					handleEventIcon={() => {
+						showPassword !== false
+							? setShowPassword(false)
+							: setShowPassword(true);
+						handleShowPassword();
+					}}
+					placeholder="Password"
+					type={showPassword !== true ? 'password' : 'text'}
+					handleInputValue={ele => console.log(ele.value)}
+				/>
+				<Input
+					icon={<PassIcon color="#776bf8" />}
+					eventIcon={passwordIcon}
+					handleEventIcon={() => {
+						showPassword !== false
+							? setShowPassword(false)
+							: setShowPassword(true);
+						handleShowPassword();
+					}}
+					placeholder="Password"
+					type={showPassword !== true ? 'password' : 'text'}
+					handleInputValue={ele => console.log(ele.value)}
+				/>
+			</div>
+
+			<div className="reset-password__button-container">
+				<Link to="/home" className="react-link">
+					<Button type="bold">Sumbit</Button>
+				</Link>
+			</div>
+		</div>
+	);
 };
