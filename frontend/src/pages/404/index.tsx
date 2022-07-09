618bee4d4a9a78f99fe51b46d67886fe7df6fff2
Andhika <91180519+AndhikaBukh@users.noreply.github.com>
2022-07-09 19:04:07 +0700

    Initial Commit

---
+++ frontend/src/pages/404/index.tsx
@@ -0,0 +1,16 @@
+export const NotFoundPage = () => {
+	return (
+		<div className="not-found">
+			<div className="not-found__content">
+				<div className="not-found__content__title">
+					<div className="not-found__content__title__text">404</div>
+				</div>
+				<div className="not-found__content__description">
+					<div className="not-found__content__description__text">
+						Page not found
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
