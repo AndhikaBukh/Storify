f119f20d13421a4bd83b8729936fd171bcb38f2f
tejuslemonnn <tejuslemonnn@gmail.com>
2022-07-12 23:51:51 +0700

    fixed CORS

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,11 +13,11 @@ const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 connectDB();
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:4000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:4000"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -31,6 +32,7 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
     }
 }))
+app.use(cors({ credentials: true, origin: 'http://localhost:4000' }));
 app.use(cookieParser());
 
 
