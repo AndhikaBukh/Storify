8419974213b7a3e5aa3a3fc9a077877436be5cf1
Andhika <91180519+AndhikaBukh@users.noreply.github.com>
2022-07-12 23:40:50 +0700

    CORS Fixed

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,12 @@ const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 connectDB();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:4000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
