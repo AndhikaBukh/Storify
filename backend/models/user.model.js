395ec44bb37ba95eebe4674bcdf457282ed6a8ea
ZuZu <86148109+zuzustack@users.noreply.github.com>
2022-07-17 00:39:41 +0700

    Update user.model.js

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -13,7 +13,7 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: [true, "Please fill a Username"],
-        unique: [true, "udah ada"],
+        unique: true,
         minlength: [5, "Username must be at least 5 characters"],
         trim: true,
         lowercase: true,
