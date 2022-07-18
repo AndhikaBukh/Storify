44b241eb70f9b288d3c3747575ae3b2207bce6fc
Andhika <91180519+AndhikaBukh@users.noreply.github.com>
2022-07-18 23:38:38 +0700

    Updated Pages

--- frontend/src/pages/404/index.tsx
+++ frontend/src/pages/404/index.tsx
@@ -1,12 +1,41 @@
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { BackIcon } from '../../components/icons';
+import { Navbar } from '../../components/navbar/navbar';
+import { Seperator } from '../../components/seperator/seperator';
+import './index.css';
+
 export const NotFoundPage = () => {
+	useEffect(() => {
+		document.title = 'Project Sylly - Not Found';
+	});
+
 	return (
 		<div className="not-found">
-			<div className="not-found__content">
-				<div className="not-found__content__title">
-					<div className="not-found__content__title__text">404</div>
-				</div>
-				<div className="not-found__content__description">
-					<div className="not-found__content__description__text">
+			<Navbar
+				className="not-found--navbar"
+				type="top"
+				topNavbarAttributes={{
+					leftContent: (
+						<>
+							<Link to="/" className="navbar__button">
+								<BackIcon />
+							</Link>
+							404 Page
+						</>
+					),
+				}}
+			/>
+
+			<div className="not-found__wrapper">
+				<div className="not-found__content">
+					<div className="not-found__content__title">404</div>
+
+					<div className="not-found__content__seperator">
+						<Seperator type="vertical" borderWidth={2} fade />
+					</div>
+
+					<div className="not-found__content__description">
 						Page not found
 					</div>
 				</div>
