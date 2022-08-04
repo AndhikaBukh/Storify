ab1332fa8aa2924643139a511066ed66bb0936cf
Andhika <91180519+AndhikaBukh@users.noreply.github.com>
2022-08-04 09:55:48 +0700

    On-Todo List - Search Page 0/1

--- frontend/src/pages/private/search/index.tsx
+++ frontend/src/pages/private/search/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { SearchIcon } from '../../../components/icons';
+import { Input } from '../../../components/input/input';
 import { Navbar } from '../../../components/navbar/navbar';
 import { useAuth } from '../../../utils/auth';
 import './index.css';
@@ -8,14 +10,30 @@ export const SearchPage = () => {
 	const auth = useAuth();
 
 	useEffect(() => {
-		auth?.requireLogin();
-
 		document.title = 'Project Sylly - Search';
 	}, []);
 
 	return (
 		<div className="search">
-			<Navbar type="top" />
+			<Navbar
+				className="search--navbar"
+				type="top"
+				topNavbarAttributes={{
+					leftContent: (
+						<Input
+							className="navbar__search"
+							type="search"
+							placeholder="Search . . ."
+							eventIcon={
+								<SearchIcon className="navbar__search__icon" />
+							}
+						/>
+					),
+					rightContent: undefined,
+				}}
+			/>
+
+			<div className="search__wrapper"></div>
 		</div>
 	);
 };
