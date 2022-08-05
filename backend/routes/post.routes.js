d8a1949168f9401ed0bed6a3799cf806c9b7e962
Vito Ramadhan <tejuslemonnn@gmail.com>
2022-08-05 23:07:47 +0700

    getPostOfFollowing Fixed

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,26 +1,31 @@
-const router = require('express').Router();
-const postCtrl = require('../controllers/post.controller');
-const auth = require('../middleware/auth');
+const router = require("express").Router();
+const postCtrl = require("../controllers/post.controller");
+const auth = require("../middleware/auth");
 
-const upload = require('../utils/multer');
+const upload = require("../utils/multer");
 
-router.route('/post') // http://localhost:3000/api/post
-    .post(auth, upload.single('images'), postCtrl.createPost) // create a post
+router
+    .route("/post") // http://localhost:3000/api/post
+    .post(auth, upload.single("images"), postCtrl.createPost) // create a post
     .get(auth, postCtrl.getAllPost); // get all post
 
-router.route('/post/:id') // http://localhost:3000/api/post/:id
+router
+    .route("/post/:id") // http://localhost:3000/api/post/:id
     .get(auth, postCtrl.getPostDetail) // get a post by id
     .post(auth, postCtrl.saveUnsavePost) // save or unsave a post
     .put(auth, postCtrl.updatePost) // update a post
     .delete(auth, postCtrl.deletePost); // delete a post
 
-router.route('/post/:id/like') // http://localhost:3000/api/post/:id/like
+router
+    .route("/post/:id/like") // http://localhost:3000/api/post/:id/like
     .put(auth, postCtrl.likePost); // like a post
 
-router.route('/post/:id/unlike') // http://localhost:3000/api/post/:id/unlike
+router
+    .route("/post/:id/unlike") // http://localhost:3000/api/post/:id/unlike
     .put(auth, postCtrl.unlikePost); // unlike a post
 
-router.route('/post/following') // http://localhost:3000/api/post/following
+router
+    .route("/posts/following") // http://localhost:3000/api/post/following
     .get(auth, postCtrl.getPostOfFollowing); // get all post of following
 
 module.exports = router;
